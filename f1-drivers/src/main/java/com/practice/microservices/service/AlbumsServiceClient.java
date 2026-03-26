f64d8580fc6a836cfce73a9a6a3cc47b0f34ea35
Pratik M <pratikmalgundkar1@gmail.com>
2026-03-27 01:00:29 +0530

    61. Implement Resilience4j Circuit Breaker, with actuator, and @Retry

--- f1-drivers/src/main/java/com/practice/microservices/service/AlbumsServiceClient.java
+++ f1-drivers/src/main/java/com/practice/microservices/service/AlbumsServiceClient.java
@@ -1,5 +1,6 @@
 package com.practice.microservices.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
@@ -8,9 +9,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.practice.microservices.model.AlbumResponseModel;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @FeignClient(name = "f1-albums")
 public interface AlbumsServiceClient {
 
 	@GetMapping("/drivers/{driverId}/albums")
+	@CircuitBreaker(name = "f1-albums", fallbackMethod = "getAlbumsFallback")
 	public List<AlbumResponseModel> getAlbums(@PathVariable String driverId);
+	
+	default List<AlbumResponseModel> getAlbumsFallback(String driverId, Throwable exception) {
+		System.out.println("Param: " + driverId);
+		System.out.println("Exception: " + exception.getMessage());
+		return new ArrayList<>();
+	}
 }
